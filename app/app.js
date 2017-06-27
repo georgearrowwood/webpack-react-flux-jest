2fb0b09cf29abff73b176cec5298a826466637a7
Yuri K <yuri@yuri-mac2.home>
2017-06-27 12:49:11 +0100

    router 4 worked somehow

--- app/app.js
+++ app/app.js
@@ -1,10 +1,31 @@
-import React from 'react'
-import {render} from 'react-dom'
-import {Router, match, browserHistory} from 'react-router';
+import React from 'react';
+// using NavLink for the activeClassName, otherwise, for
+// a plain link, use Link.
+import { Route, NavLink } from 'react-router-dom';
 
-const routes = require ('./routes').default;
+import Home from './home';
+// import Complex from '~/routes/Complex';
+// import About from '~/routes/About';
 
-render(
-  <Router history={browserHistory} routes={routes} />,
-  document.getElementById('app')
-);
+// import styles from './App.scss';
+
+// Our Pure and clean App component. In the browser, we'll wrap
+// this inside a BrowserRouter component and put it in the DOM,
+// otherwise on server render, we'll stick this in a StaticRouter
+// component and renderToString it.
+
+const App = () => (
+    <div >
+        <header>
+            <h1> A demo site. </h1>
+        </header>
+        <nav>
+            <NavLink exact to="/" > home </NavLink>
+        </nav>
+        <main>
+            <Route exact path="/" component={ Home }/>
+        </main>
+    </div>
+)
+
+module.exports = App;
--- server/app/routes/app.js
+++ server/app/routes/app.js
@@ -1,43 +1,37 @@
 import express from 'express';
 let router = express.Router();
 
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { match, RouterContext } from 'react-router';
-// import { matchRoutes, renderRoutes } from 'react-router-config'
-const { StaticRouter } = require('react-router-dom');
-const { createElement: h } = require('react');
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
+import { StaticRouter } from 'react-router'
 
 import config from '../config'; 
-import Routes from '../../../app/routes';
-import App from '../../../app/client';
+// import Routes from '../../../app/routes';
+import App from '../../../app/app';
+
+import fs from 'fs'
+const index = fs.readFileSync('./index.html', 'utf8')
 
 router.get('*', (req, res) => {
 
-  const appHtml = renderToString(
+  const context = {}
 
-      h(StaticRouter, { location: req.url, context: {} },
-        h(App)));
-  console.log(appHtml);
-  // matchRoutes(Routes, req.url).then( (err, redirect, props) => {
-  //
-  //   if (err) {
-  //     res.status(500).send(err.message)
-  //   } else if (redirect) {
-  //     res.redirect(redirect.pathname + redirect.search)
-  //   } else if (props) {
-  //     const appHtml = renderToString(<RouterContext {...props}/>)
-  //     // res.send(renderPage(appHtml))
-  //     res.render('html.ejs', {
-  //       title: config.title,
-  //       app: appHtml,
-  //       scriptUrl: config.scriptUrl,
-  //       styleUrl: config.styleUrl
-  //     });
-  //   } else {
-  //     res.status(404).send('Not Found')
-  //   }
-  // })
+  const html = ReactDOMServer.renderToString(
+    <StaticRouter location={req.url} context={context}>
+      <App/>
+    </StaticRouter>
+  )
+
+  if (context.url) {
+    res.writeHead(301, {Location: context.url})
+    res.end()
+  } else {
+    res.write(index.replace(
+        /<div id="root"><\/div>/,
+		`<div id="root">${html}</div>`
+    ));
+    res.end()
+  }
 })
 
 module.exports = router;
